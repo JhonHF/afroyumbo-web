3b895d75660ca993c6a9046b0548e65b8a190ecd
@JhonHF <hamilton5217@gmail.com>
2025-05-18 22:37:48 -0500

    add home content and connect blog with headless api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Navbar from "@/features/navbar/ui/navbar";
 import HomeContent from "@/features/homeContent/ui/HomeContent";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <HomeContent />
-    </div>
-  );
+  return <HomeContent />;
 }
--- src/features/homeContent/ui/HomeContent.tsx
+++ src/features/homeContent/ui/HomeContent.tsx
@@ -3,9 +3,7 @@ import React from "react";
 
 export default function HomeContent() {
   return (
-    <main className="relative flex-1">
-      {/* Cambia h-screen por flex-1 */}
-      {/* Contenedor de la imagen de fondo */}
+    <div className="relative grid h-full place-items-center">
       <div className="absolute inset-0 -z-10">
         <Image
           src="/desfile_de_la_afrocolombianidad.jpg"
@@ -15,12 +13,18 @@ export default function HomeContent() {
           quality={100}
         />
       </div>
-      {/* Overlay semitransparente */}
-      <div className="absolute inset-0 bg-black/70 -z-10"></div>
-      {/* Contenido principal (centrado) */}
-      <div className="flex items-center justify-center h-full">
-        <h1 className="text-white text-4xl font-bold">texto</h1>
+      <div className="absolute inset-0 bg-black/70 -z-10" />
+      {/* Contenido centrado */}
+      <div className="text-center px-4">
+        <h1 className="text-white text-4xl font-bold">
+          Bienvenidos a afroyumbo
+        </h1>
+
+        <h2 className="text-white text-2xl font-bold mt-16">
+          Somos una organizacion que promueve y vela por los derechos de los
+          afrocolombianos
+        </h2>
       </div>
-    </main>
+    </div>
   );
 }
